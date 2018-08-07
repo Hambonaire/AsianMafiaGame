e3218abc8d3b78aa095cc09a570084827daba4f8
Orion <borionwaffles@gmail.com>
2018-08-07 16:11:48 -0400

    Recreate noise and Map Gen stuff
    All my stuff disappeared from last time so doing another commi to put it back.

---
+++ AsianMafiaGame/Assets/Scripts/MapGenStuff/MapGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+    public bool autoUpdate;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
