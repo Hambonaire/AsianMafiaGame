196f5faf64bcc1aea8ddf033115f4cdae6516054
Orion <borionwaffles@gmail.com>
2018-08-13 22:40:36 -0400

    Basic movement and player
    thE baS1cZ

---
+++ AsianMafiaGame/Assets/Scripts/PlayerCentric/Temp3rdPerson/CameraControllerrrrr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerrrrr : MonoBehaviour {
+
+
+    // Use this for initialization
+    public Transform playerTransform;
+
+    public float speedV = 2.0f;
+    private float pitch = 0.0f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        transform.rotation = playerTransform.rotation;
+        transform.position = playerTransform.position;
+        */
+        pitch += speedV * Input.GetAxis("Mouse Y");
+
+        //transform.eulerAngles = new Vector3(playerTransform.eulerAngles.y, playerTransform.eulerAngles.x , playerTransform.eulerAngles.z);
+    }
+}
---
+++ AsianMafiaGame/Assets/Scripts/PlayerCentric/Temp3rdPerson/CharacterControllerrrr.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerrrr : MonoBehaviour {
+
+    // Use this for initialization
+    public Rigidbody rb;
+    public float speed;
+    public float velocityClamp;
+    public float speedH = 2.0f;
+    public float jumpForce;
+    public float jumpCooldown;
+    public GameObject AttackOne;
+    public float attackCooldown;
+    private Vector3 rbFrontPos; // the position where the attack comes out of character
+    public float rbFrontYPos; //adjustable y pos for Vector3 ^
+
+    private float moveHorizontal = 0.0f;
+    private float moveVertical = 0.0f;
+    private float yaw = 0.0f;
+    private bool cdJumpActive;
+    private bool cdAttackActive;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+        Rotation();
+        CheckSpeed();
+        JumpCheck();
+        AttackCheck();
+    }
+
+    void Movement()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        movement = Camera.main.transform.TransformDirection(movement);
+        movement.y = 0.0f;
+        rb.velocity += movement * speed;
+    }
+    void CheckSpeed()
+    {
+        if (rb.velocity.magnitude > velocityClamp)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, velocityClamp);
+        }
+    }
+
+    void Rotation()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+
+
+        transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(20, 20, 200, 200), "rigidbody velocity: " + rb.velocity);
+    }
+
+    void JumpCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (cdJumpActive == false)
+            {
+                rb.AddForce(0.0f, jumpForce, 0.0f);
+                Invoke("ResetJump", jumpCooldown);
+                cdJumpActive = true;
+            }
+
+        }
+    }
+    void ResetJump()
+    {
+        cdJumpActive = false;
+    }
+
+    void AttackCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (cdAttackActive == false)
+            {
+
+                rbFrontPos.x = rb.position.x;
+                rbFrontPos.y = rb.position.y + rbFrontYPos;
+                rbFrontPos.z = rb.position.z;
+
+                Instantiate(AttackOne, rbFrontPos, rb.rotation);
+                Invoke("ResetAttack", attackCooldown);
+                cdAttackActive = true;
+            }
+        }
+    }
+    void ResetAttack()
+    {
+        cdAttackActive = false;
+    }
+}
